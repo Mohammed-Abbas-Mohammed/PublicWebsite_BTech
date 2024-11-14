bddccf6c8602a19d144c98b1ac42e49e0278a8eb
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-14 21:33:26 +0200

    localization my account and change style

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -2,22 +2,31 @@ import { Component } from '@angular/core';
 import { OrderService } from '../service/Order/order.service';
 import { AuthService } from '../service/Identity/auth.service';
 import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalizationService } from '../service/localiztionService/localization.service';
 
 @Component({
   selector: 'app-my-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './my-orders.component.html',
-  styleUrl: './my-orders.component.css'
+  styleUrl: './my-orders.component.css',
 })
 export class MyOrdersComponent {
+  isArabic!: boolean;
+
   userId: string | null = null; // User ID retrieved from token
   orders: any[] = [];
   loading = true;
   error: string | null = null;
-  constructor(private orderService: OrderService,
-   private authservice: AuthService
-  ) {}
+  constructor(
+    private orderService: OrderService,
+    private authservice: AuthService,
+    private translate: LocalizationService,
+    private translator: TranslateService
+  ) {
+    this.translate.IsArabic.subscribe((ar) => (this.isArabic = ar));
+  }
 
   ngOnInit(): void {
     this.userId = this.authservice.getUserIdNourhan(); // Retrieve the user ID from token
@@ -74,11 +83,11 @@ export class MyOrdersComponent {
   }
 
   getStatus(status: number): string {
-    switch (status) {
-      case 1: return 'Processing';
-      case 2: return 'Shipped';
-      case 3: return 'Completed';
-      default: return 'Unknown';
-    }
+    return this.translator.instant(`orders.status.${status}`) || this.translator.instant('orders.status.default');
+}
+
+  getFormattedPrice(price: number): string {
+    const formattedPrice = price.toFixed(2); // Ensures 2 decimal places
+    return this.isArabic ? `${formattedPrice} ج.م` : `EGP ${formattedPrice}`;
   }
 }
